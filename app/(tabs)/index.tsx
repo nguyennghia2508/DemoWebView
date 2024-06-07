f4afc80d550c0d5f3b76f1057bfe65c89e8e7ce1
Nguyen Thanh Nghia <binlatao3@gmail.com>
2024-06-07 10:31:41 +0700

    update refresh page by pulling down

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
-import React, { useRef, useState } from 'react';
-import { StyleSheet, View, RefreshControl, ScrollView, Dimensions } from 'react-native';
+import React, { useState, useCallback, useEffect, useRef } from 'react';
+import { StyleSheet, View, RefreshControl, ScrollView, Dimensions, Alert, BackHandler, Platform } from 'react-native';
 import WebView from 'react-native-webview';
 
 const initialUrl = 'https://tasken.io';
@@ -7,21 +7,75 @@ const initialUrl = 'https://tasken.io';
 export default function HomeScreen() {
   const [refreshing, setRefreshing] = useState(false);
   const [key, setKey] = useState(0);
-  const [url, setUrl] = useState(initialUrl)
-  
-  const onRefresh = React.useCallback(() => {
+  const [url, setUrl] = useState(initialUrl);
+  const [scrollEnabled, setScrollEnabled] = useState(true);
+  const webViewRef = useRef<WebView>(null);
+
+  const onRefresh = useCallback(() => {
     setRefreshing(true);
-    setKey((prevKey) => prevKey + 1); // Update the key to reload the WebView
+    setKey(prevKey => prevKey + 1);
     setTimeout(() => {
       setRefreshing(false);
     }, 1000);
   }, []);
 
   const onNavigationStateChange = (navState: any) => {
-    if (url!==navState.url) { 
-      setUrl(navState.url)
-      alert(navState.url)
+    if (url !== navState.url) { 
+      setUrl(navState.url);
+      console.log(navState.url);
     }
+  };
+
+  const onError = (syntheticEvent: any) => {
+    const { nativeEvent } = syntheticEvent;
+    Alert.alert(
+      'Webview Error',
+      `Error code: ${nativeEvent.code}\nDescription: ${nativeEvent.description}`,
+      [{ text: 'OK' }]
+    );
+  };
+
+  const onHttpError = (syntheticEvent: any) => {
+    const { nativeEvent } = syntheticEvent;
+    Alert.alert(
+      'HTTP Error',
+      `HTTP error: ${nativeEvent.statusCode}`,
+      [{ text: 'OK' }]
+    );
+  };
+
+  const handleScroll = (event: any) => {
+    const { contentOffset } = event.nativeEvent;
+    if (contentOffset.y === 0) {
+      setScrollEnabled(true);
+    } else {
+      setScrollEnabled(false);
+    }
+  };
+
+  const handleBackPress = () => {
+    if (webViewRef.current) {
+      webViewRef.current.goBack();
+    }
+    return false;
+  };
+
+  useEffect(() => {
+    const backHandler = BackHandler.addEventListener('hardwareBackPress', handleBackPress);
+    return () => backHandler.remove();
+  }, []);
+
+  const onShouldStartLoadWithRequest = (request: any) => {
+    const { url } = request;
+    // Block navigation to the specified URL
+    if (url.includes('https://365vn.sharepoint.com')) {
+      setTimeout(() => {
+        if (webViewRef.current) {
+          webViewRef.current.stopLoading();
+        }
+      }, 100);
+    }
+    return true;
   };
 
   return (
@@ -29,21 +83,42 @@ export default function HomeScreen() {
       <ScrollView
         contentContainerStyle={styles.scrollView}
         refreshControl={
-          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          Platform.select({
+           ios: (
+            <RefreshControl 
+            refreshing={refreshing} 
+            onRefresh={onRefresh} 
+            />
+           ),
+           android: (
+            <RefreshControl
+            enabled={scrollEnabled}
+            refreshing={refreshing} 
+            onRefresh={onRefresh} 
+            />
+           )
+          })
         }
+        scrollEnabled={scrollEnabled}
       >
         <View style={styles.webviewContainer}>
           <WebView
             key={key}
-            source={{
-              uri: url,
-            }}
-            allowsFullscreenVideo={true}
-            javaScriptEnabled={true}
-            domStorageEnabled={true}
-            startInLoadingState={true}
+            ref={webViewRef}
+            source={{ uri: url }}
+            sharedCookiesEnabled
+            thirdPartyCookiesEnabled
+            allowsFullscreenVideo
+            javaScriptEnabled
+            domStorageEnabled
+            startInLoadingState
+            userAgent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/119.0.2151.97"
             style={styles.webview}
             onNavigationStateChange={onNavigationStateChange}
+            onError={onError}
+            onHttpError={onHttpError}
+            onScroll={handleScroll}
+            onShouldStartLoadWithRequest={onShouldStartLoadWithRequest}
           />
         </View>
       </ScrollView>
@@ -54,11 +129,11 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 50,
+    marginTop: 75,
   },
   webviewContainer: {
     flex: 1,
-    height: Dimensions.get('window').height - 50, // Adjust to account for the marginTop
+    height: Dimensions.get('window').height - 75,
   },
   webview: {
     flex: 1,
